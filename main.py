7dddb5357ecaecb33bc86c088b01a61bc7998d0e
Neeraj Bhatt <neeraj1bh@gmail.com>
2024-02-27 23:07:35 +0530

    chore: added report tool and schema for tools

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ from langchain.agents import (
 from dotenv import load_dotenv
 
 from tools.sql import run_query_tool, list_tables, describe_tables_tool
+from tools.report import write_report_tool
 
 load_dotenv()
 
@@ -24,14 +25,19 @@ tables = list_tables()
 prompt = ChatPromptTemplate(
     messages=[
         SystemMessage(
-            content=f"You are an AI that has access to a Sqlite database. \n{tables}"
+            content=(
+                "You are an AI that has access to a Sqlite database. \n"
+                f"The database has table of: {tables}"
+                "Do not make any assumptions of what tables or columns exist."
+                "Instead use the 'describe_tables' function."
+            )
         ),
         HumanMessagePromptTemplate.from_template("{input}"),
         MessagesPlaceholder(variable_name="agent_scratchpad"),
     ]
 )
 
-tools = [run_query_tool, describe_tables_tool]
+tools = [run_query_tool, describe_tables_tool, write_report_tool]
 
 # agent_executor = initialize_agent(
 #     llm=chat, tools=tools, agent=AgentType.OPENAI_FUNCTIONS, verbose=True
@@ -44,4 +50,8 @@ agent = OpenAIFunctionsAgent(llm=chat, prompt=prompt, tools=tools)
 agent_executor = AgentExecutor(agent=agent, verbose=True, tools=tools)
 
 
-agent_executor.invoke("How many users have provided a shipping address?")
+# agent_executor.invoke("How many users have provided a shipping address?")
+
+agent_executor.invoke(
+    "Summarize the top 5 products. Write the results to a report file"
+)
