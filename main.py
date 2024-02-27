1d9ac03eeabe82b07c1647486427fea8bf875cb5
Neeraj Bhatt <neeraj1bh@gmail.com>
2024-02-27 23:59:34 +0530

    chore: added memory

--- main.py
+++ main.py
@@ -12,14 +12,17 @@ from langchain.agents import (
     AgentType,
     create_openai_functions_agent,
 )
+from langchain.memory import ConversationBufferMemory
 from dotenv import load_dotenv
 
 from tools.sql import run_query_tool, list_tables, describe_tables_tool
 from tools.report import write_report_tool
+from handlers.chat_model_start_handler import ChatModelStartHandler
 
 load_dotenv()
 
-chat = langchain_openai.ChatOpenAI()
+handler = ChatModelStartHandler()
+chat = langchain_openai.ChatOpenAI(callbacks=[handler])
 
 tables = list_tables()
 prompt = ChatPromptTemplate(
@@ -32,11 +35,13 @@ prompt = ChatPromptTemplate(
                 "Instead use the 'describe_tables' function."
             )
         ),
+        MessagesPlaceholder(variable_name="chat_history"),
         HumanMessagePromptTemplate.from_template("{input}"),
         MessagesPlaceholder(variable_name="agent_scratchpad"),
     ]
 )
 
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
 tools = [run_query_tool, describe_tables_tool, write_report_tool]
 
 # agent_executor = initialize_agent(
@@ -47,11 +52,20 @@ agent = OpenAIFunctionsAgent(llm=chat, prompt=prompt, tools=tools)
 
 # agent = create_openai_functions_agent(llm=chat, prompt=prompt, tools=tools)
 
-agent_executor = AgentExecutor(agent=agent, verbose=True, tools=tools)
+agent_executor = AgentExecutor(
+    agent=agent,
+    # verbose=True,
+    tools=tools,
+    memory=memory,
+)
 
 
 # agent_executor.invoke("How many users have provided a shipping address?")
 
-agent_executor.invoke(
-    "Summarize the top 5 products. Write the results to a report file"
-)
+# agent_executor.invoke(
+#     "Summarize the top 5 products. Write the results to a report file"
+# )
+
+agent_executor.invoke("How many orders are there? Write the results to an html report.")
+
+agent_executor.invoke("Repeat the exact same process for users.")
