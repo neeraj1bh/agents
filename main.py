fd1f117b5ab29cd0d24a9f5568e3ad1b3bc696a8
Neeraj Bhatt <neeraj1bh@gmail.com>
2024-02-27 22:20:24 +0530

    chore: added new tools and db commands for correct response

--- main.py
+++ main.py
@@ -4,29 +4,34 @@ from langchain.prompts import (
     HumanMessagePromptTemplate,
     MessagesPlaceholder,
 )
+from langchain.schema import SystemMessage
 from langchain.agents import (
     OpenAIFunctionsAgent,
     AgentExecutor,
     initialize_agent,
     AgentType,
+    create_openai_functions_agent,
 )
 from dotenv import load_dotenv
 
-from tools.sql import run_query_tool
+from tools.sql import run_query_tool, list_tables, describe_tables_tool
 
 load_dotenv()
 
 chat = langchain_openai.ChatOpenAI()
 
-
+tables = list_tables()
 prompt = ChatPromptTemplate(
     messages=[
+        SystemMessage(
+            content=f"You are an AI that has access to a Sqlite database. \n{tables}"
+        ),
         HumanMessagePromptTemplate.from_template("{input}"),
         MessagesPlaceholder(variable_name="agent_scratchpad"),
     ]
 )
 
-tools = [run_query_tool]
+tools = [run_query_tool, describe_tables_tool]
 
 # agent_executor = initialize_agent(
 #     llm=chat, tools=tools, agent=AgentType.OPENAI_FUNCTIONS, verbose=True
@@ -34,7 +39,9 @@ tools = [run_query_tool]
 
 agent = OpenAIFunctionsAgent(llm=chat, prompt=prompt, tools=tools)
 
+# agent = create_openai_functions_agent(llm=chat, prompt=prompt, tools=tools)
+
 agent_executor = AgentExecutor(agent=agent, verbose=True, tools=tools)
 
 
-agent_executor("How many users are there in the database?")
+agent_executor.invoke("How many users have provided a shipping address?")
--- tools/sql.py
+++ tools/sql.py
@@ -4,12 +4,38 @@ from langchain.tools import Tool
 conn = sqlite3.connect("db.sqlite")
 
 
+def list_tables():
+    c = conn.cursor()
+    c.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    rows = c.fetchall()
+    return "\n".join(row[0] for row in rows if row[0] is not None)
+
+
 def run_sqlite_query(query):
     c = conn.cursor()
-    c.execute(query)
-    return c.fetchall()
+    try:
+        c.execute(query)
+        return c.fetchall()
+    except sqlite3.OperationalError as err:
+        return f"The following error occured: {str(err)}"
 
 
 run_query_tool = Tool.from_function(
     name="run_sqlite_query", description="Run a sqlite query", func=run_sqlite_query
+)
+
+
+def describe_tables(table_names):
+    c = conn.cursor()
+    tables = ", ".join("'" + table + "'" for table in table_names)
+    rows = c.execute(
+        f"SELECT sql from sqlite_master WHERE type='table' and name IN ({tables});"
+    )
+    return "\n".join(row[0] for row in rows if row[0] is not None)
+
+
+describe_tables_tool = Tool.from_function(
+    name="describe_tables",
+    description="Given a list of table names, return the schema of those tables.",
+    func=describe_tables,
 )
